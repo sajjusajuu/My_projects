bc1edb5fa6cb833cefb0de3795512c675b2a6b58
Sajid Hussain <sajidhussain2803@gmail.com>
2014-08-12 21:31:09 +0530

    First commit

---
+++ linkedlist/pro11.cpp
@@ -0,0 +1,54 @@
+// Cloning a list with one pointer pointing to next node and other pointer pointing to random node
+
+
+// Written by Sajid Hussain, ISM DhanBad.
+
+
+
+#include <iostream>
+#include <cstdlib>
+using namespace std;
+
+
+class listnode {
+	public:
+	int data;
+	listnode * ptr;
+	listnode(int n) { data = n;  ptr = NULL;}
+	listnode() { data = 0; ptr = NULL; }
+};
+
+listnode* createlist(int n) {
+	
+	listnode * start = NULL;
+	listnode *pre = NULL;
+	if(n == 0)
+		return start;
+	start = new listnode(0);
+	pre = start;
+	for(int i = 1; i<n; i++) { 
+		pre->ptr = new listnode(i);
+		pre = pre->ptr;
+	}
+	return start;
+}
+	
+		
+		
+
+int main() {
+	
+	listnode* l = createlist(100);	// Creates a linkedlist of 100 nodes and returs the pointers to the first node
+
+	/*while(l) {
+		cout << l->data << " ";
+		l = l->ptr;
+	}*/
+	
+
+	
+	
+	return 0;
+}
+	
+	
