73b7c21b66c054bd855cf5c37bf0657e8610fef2
Sajid Hussain <sajidhussain2803@gmail.com>
2014-08-20 22:20:52 +0530

    First commit

---
+++ Trees/pro4.cpp
@@ -0,0 +1,138 @@
+// searching an element in a tree using recursion
+
+// This also contains the level order traversal of a tree
+
+// Written by Sajid Hussain
+
+#include <iostream>
+#include <cstdlib>
+#include <cstdio>
+using namespace std;
+
+class treenode  {
+	public:
+	int data;	
+	treenode *right;
+	treenode *left;
+	treenode(int i) { data = i; right = left = NULL; }
+};
+
+class queuenode {
+	public:
+	treenode *data;
+	queuenode *next;
+	queuenode(treenode *p) {
+		data = p;
+		next = NULL;
+	}
+};
+
+
+class queue {
+	public:
+	queuenode *front, *rear;
+	queue() { front = rear = NULL; }
+	void enque(treenode *ptr) {
+		if(front == NULL) {
+			front = new queuenode(ptr);
+			rear = front;
+		}
+		else {
+		rear->next = new queuenode(ptr);
+		rear = rear->next;
+		}
+	}
+	treenode * deque() {
+		if(front == rear) {
+			treenode *temp = front->data;
+			rear = front = NULL;
+			return temp;
+		}
+		else {
+			treenode *temp = front->data;
+			front = front->next;
+			return temp;
+		}
+
+	}
+	bool isEmpty() {
+		return (!front);
+	}
+};
+
+
+void createtree( treenode **ptr, int n) {
+	if(n == 0)
+		return;
+	*ptr = new treenode(((float)rand()/RAND_MAX)*100);
+	n--;
+	int n1 = n/2;
+	int n2 = n - n1;
+	createtree(&((*ptr)->right), n1);
+	createtree(&((*ptr)->left), n2);
+}
+
+
+
+void printusingq(treenode *root) {
+	queue Q;
+	int count = 0;
+	Q.enque(root);
+	while(!(Q.isEmpty())) {
+		treenode *temp = Q.deque();
+		cout << temp->data << " ";
+		if(temp->left)
+			Q.enque(temp->left);
+		if(temp->right)
+			Q.enque(temp->right);
+		count++;
+		if(count > 100) break;
+	}
+	cout << endl;
+}
+	
+
+void print( treenode *root) {
+	if(!root)
+		return;
+	else {
+		cout << root->data << " ";
+		print(root->left);
+		print(root->right);
+	}
+}
+
+bool search(treenode *root , int n) {
+	if( !root)
+		return false;
+	else {
+		if(root->data == n)
+			return true;
+		else {
+			if(search(root->left, n))
+				return true;
+			if(search(root->right, n))
+				return true;
+			return false;
+		}
+	}
+}
+
+
+int main() {
+
+	treenode *root;
+	createtree(&root, 50);
+
+	print(root);
+	cout << endl;
+	
+	bool found = search(root, 2);
+
+	if(found) cout <<"That element exits in this tree" << endl;
+	else cout << "Does not exits" << endl;
+
+	return 0;
+}
+	
+	
---
+++ Trees/pro7.cpp
@@ -0,0 +1,147 @@
+// Printing the deepest node of a binary tree
+
+// This also contains the level order traversal of a tree
+
+// Written by Sajid Hussain
+
+
+
+#include <iostream>
+#include <cstdlib>
+#include <cstdio>
+using namespace std;
+
+class treenode  {
+	public:
+	int data;	
+	treenode *right;
+	treenode *left;
+	treenode(int i) { data = i; right = left = NULL; }
+};
+
+class queuenode {
+	public:
+	treenode *data;
+	queuenode *next;
+	queuenode(treenode *p) {
+		data = p;
+		next = NULL;
+	}
+};
+
+
+class queue {
+	public:
+	queuenode *front, *rear;
+	queue() { front = rear = NULL; }
+	void enque(treenode *ptr) {
+		if(front == NULL) {
+			front = new queuenode(ptr);
+			rear = front;
+		}
+		else {
+		rear->next = new queuenode(ptr);
+		rear = rear->next;
+		}
+	}
+	treenode * deque() {
+		if(front == rear) {
+			treenode *temp = front->data;
+			rear = front = NULL;
+			return temp;
+		}
+		else {
+			treenode *temp = front->data;
+			front = front->next;
+			return temp;
+		}
+
+	}
+	bool isEmpty() {
+		return (!front);
+	}
+	treenode *peep() {
+		if(front == NULL)
+			return NULL;
+		else
+			return front->data;
+	}
+		
+};
+
+
+void createtree( treenode **ptr, int n) {
+	if(n == 0)
+		return;
+	*ptr = new treenode(((float)rand()/RAND_MAX)*100);
+	n--;
+	int n1 = n/2;
+	int n2 = n - n1;
+	createtree(&((*ptr)->right), n1);
+	createtree(&((*ptr)->left), n2);
+}
+
+
+
+void printusingq(treenode *root) {
+	queue Q;
+	Q.enque(root);
+	Q.enque(NULL);
+	while(!(Q.isEmpty())) {
+		treenode *temp = Q.deque();
+		if(temp == NULL) {
+			if(!Q.isEmpty()) {
+				Q.enque(NULL);
+				cout << endl;
+				continue;
+			}
+			else
+				break;
+		}
+		cout << temp->data << " ";
+		if(temp->left)
+			Q.enque(temp->left);
+		if(temp->right)
+			Q.enque(temp->right);
+	}
+	cout << endl;
+}
+	
+
+void print( treenode *root) {
+	if(!root)
+		return;
+	else {
+		cout << root->data << " ";
+		print(root->left);
+		print(root->right);
+	}
+}
+
+
+
+int deepest(treenode *root) {
+	if(root == NULL)
+		return -1;
+	queue Q;
+	Q.enque(root);
+	treenode *temp;
+	while(!(Q.isEmpty())) {
+		temp = Q.deque();
+		if(temp->left)
+			Q.enque(temp->left);
+		if(temp->right)
+			Q.enque(temp->right);
+	}
+	return temp->data;
+}
+
+int main() {
+	
+	treenode *root;
+	createtree(&root, 50);
+	cout << "The deepest node of a binary tree " << deepest(root) <<endl;
+	return 0;
+}
+	
+	
